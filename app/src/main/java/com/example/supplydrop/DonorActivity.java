886b97d267af6ba1ce7a80b2ac9feb8696e5ff1f
Seren1Ty27 <tavish.roopnarain.27@gmail.com>
2026-05-19 17:48:58 +0200

    Comments removed and added to activity_donor.xml and DonorActivity

--- app/src/main/java/com/example/supplydrop/DonorActivity.java
+++ app/src/main/java/com/example/supplydrop/DonorActivity.java
@@ -17,14 +17,14 @@ public class DonorActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.donorBottomNav);
 
-        // Load dashboard fragment by default
+        // Load the dahsboard by default
         loadFragment(new DonorDashboardFragment());
 
-        bottomNav.setOnItemSelectedListener(item -> {
+        bottomNav.setOnItemSelectedListener(item -> { // Listener set to track swithces
             Fragment selected;
             int id = item.getItemId();
 
-            if (id == R.id.nav_dashboard) {
+            if (id == R.id.nav_dashboard) {//Assign the selected fragment
                 selected = new DonorDashboardFragment();
             } else if (id == R.id.nav_account) {
                 selected = new DonorAccountFragment();
@@ -32,24 +32,23 @@ public class DonorActivity extends AppCompatActivity {
                 return false;
             }
 
-            loadFragment(selected);
+            loadFragment(selected);//Load the new screen to th fragment
             return true;
         });
-        getOnBackPressedDispatcher().addCallback(this,
-                new androidx.activity.OnBackPressedCallback(true) {
-                    @Override
-                    public void handleOnBackPressed() {
-                        BottomNavigationView bottomNav = findViewById(R.id.donorBottomNav);
-                        if (bottomNav.getSelectedItemId() == R.id.nav_account) {
-                            // On account → go to dashboard
-                            bottomNav.setSelectedItemId(R.id.nav_dashboard);
-                        }
-                        // On dashboard → do nothing (back is blocked)
-                    }
-                });
+        getOnBackPressedDispatcher().addCallback(this, new androidx.activity.OnBackPressedCallback(true) {
+            //Creates the queue to backtrack when the back button is pressed
+            @Override
+            public void handleOnBackPressed() {
+                BottomNavigationView bottomNav = findViewById(R.id.donorBottomNav);
+                if (bottomNav.getSelectedItemId() == R.id.nav_account) {
+                    // Go to Dashboard
+                    bottomNav.setSelectedItemId(R.id.nav_dashboard);
+                }//Dont need to do anything else otherwise
+            }
+        });
     }
 
-    private void loadFragment(Fragment fragment) {
+    private void loadFragment(Fragment fragment) {//Load fragment function
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.donorFragmentContainer, fragment);
